6c3500bd8c746119152979be950586ae9f30dce3
Charif Chaibainou <chaibainoucharif@gmail.com>
2018-03-24 12:43:26 +0100

    web scrapers

---
+++ multi_scraper_allocine.py
@@ -0,0 +1,120 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+from time import sleep, time
+from random import randint
+from IPython.core.display import clear_output
+from warnings import warn
+
+
+# In[6]:
+
+
+pages_url = [str(i) for i in range(1,5)]
+annees_url = [str(i) for i in range(2010,2017)]
+
+# Lists to store the scraped data in
+names=[]
+dates=[]
+producers=[]
+actors=[]
+press_ratings=[]
+spectators_ratings=[]
+
+# Preparing the monitoring of the loop
+start_time = time()
+requests = 0
+
+# For every year in the interval 2015-2018
+for annee_url in annees_url:
+
+    # For every page in the interval 1-3
+    for page_url in pages_url:
+
+        # Make a get request
+        response = get('http://www.allocine.fr/films/decennie-2010/annee-' + annee_url + '/?page=' + page_url)
+
+        # Pause the loop
+        sleep(randint(8,15))
+
+        # Monitor the requests
+        requests += 1
+        elapsed_time = time() - start_time
+        print('Request:{}; Frequency: {} requests/s'.format(requests, requests/elapsed_time))
+        clear_output(wait = True)
+
+        # Throw a warning for non-200 status codes
+        if response.status_code != 200:
+            warn('Request: {}; Status code: {}'.format(requests, response.status_code))
+
+        # Break the loop if the number of requests is greater than expected
+        if requests > 3:
+            warn('Number of requests was greater than expected.')  
+            break 
+
+        # Parse the content of the request with BeautifulSoup
+        page_html = BeautifulSoup(response.text, 'html.parser')
+
+        # Select all the 15 movie containers from a single page
+        movie_containers = page_html.find_all('div', class_ = 'card card-entity card-entity-list cf')
+
+        # For every movie of these 15, extract data from individual movie container
+        for container in movie_containers:
+
+            rating_box=container.find('div', class_='rating-holder').find_all('div', class_='rating-item')
+
+            # If the movie has ratings, then extract:
+            if rating_box != []:
+
+                # Scrape the name
+                name=container.div.h2.a.text
+                names.append(name)
+
+                # Scrape the date
+                date=container.div.div.div.span.text
+                dates.append(date)
+
+                # Scrape the producer
+                producer=container.div.div.find('div', class_='meta-body-item meta-body-direction light').span.text
+                producers.append(producer)
+
+                # Scrape the actors
+                actor=container.div.div.find('div', class_='meta-body-item meta-body-actor light').text
+                actors.append(actor)
+
+                type_rating=container.find('div', class_='rating-holder').find_all('div', class_='rating-item')[0].span.text
+                first_rating=container.find('div', class_='rating-holder').find_all('div', class_='rating-item')[0].div.find('span', class_='stareval-note').text
+                # Scrape the press rating
+                if type_rating == ' Presse ':
+                    press=first_rating
+                    press_ratings.append(press)
+                    if len(container.find('div', class_='rating-holder').find_all('div', class_='rating-item')) > 2:
+                        spectator=container.find('div', class_='rating-holder').find_all('div', class_='rating-item')[1].div.find('span', class_='stareval-note').text
+                        spectators_ratings.append(spectator)
+                    else:
+                        spectators_ratings.append('\n                0')
+                # Scrape the spectators rating
+                else:
+                    spectator=first_rating
+                    spectators_ratings.append(spectator)
+                    press_ratings.append('\n                0')
+
+
+# In[7]:
+
+
+test_df = pd.DataFrame({'movie': names,
+                       'date': dates,
+                       'producer': producers,
+                       'actor': actors,
+                       'press_rating': press_ratings,
+                       'spectators_rating' : spectators_ratings})
+print(test_df.info())
+test_df.head(15)
+
---
+++ scraper_allocine.py
@@ -0,0 +1,100 @@
+
+# coding: utf-8
+
+# In[94]:
+
+
+from requests import get
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# In[95]:
+
+
+url='http://www.allocine.fr/films/genre-13025/decennie-2010/annee-2017/'
+
+
+# In[96]:
+
+
+response=get(url)
+
+
+# In[97]:
+
+
+html_soup = BeautifulSoup(response.text, 'html.parser')
+
+
+# In[98]:
+
+
+movie_containers = html_soup.find_all('div', class_ = 'card card-entity card-entity-list cf')
+
+
+# In[99]:
+
+
+# Lists to store the scraped data in
+names=[]
+dates=[]
+producers=[]
+actors=[]
+press_ratings=[]
+spectators_ratings=[]
+
+# Extract data from individual movie container
+for container in movie_containers:
+
+    rating_box=container.find('div', class_='rating-holder').find_all('div', class_='rating-item')
+    
+    # If the movie has ratings, then extract:
+    if rating_box != []:
+
+        # The name
+        name=container.div.h2.a.text
+        names.append(name)
+        
+        # The date
+        date=container.div.div.div.span.text
+        dates.append(date)
+        
+        # The producer
+        producer=container.div.div.find('div', class_='meta-body-item meta-body-direction light').span.text
+        producers.append(producer)
+        
+        # The actors
+        actor=container.div.div.find('div', class_='meta-body-item meta-body-actor light').text
+        actors.append(actor)
+
+        type_rating=container.find('div', class_='rating-holder').find_all('div', class_='rating-item')[0].span.text
+        first_rating=container.find('div', class_='rating-holder').find_all('div', class_='rating-item')[0].div.find('span', class_='stareval-note').text
+        # The press rating
+        if type_rating == ' Presse ':
+            press=first_rating
+            press_ratings.append(press)
+            if len(container.find('div', class_='rating-holder').find_all('div', class_='rating-item')) == 2:
+                spectator=container.find('div', class_='rating-holder').find_all('div', class_='rating-item')[1].div.find('span', class_='stareval-note').text
+                spectators_ratings.append(spectator)
+            else:
+                spectators_ratings.append('\n                0')
+        # The spectators rating
+        elif type_rating == ' Spectateurs ':
+            spectator=first_rating
+            spectators_ratings.append(spectator)
+            press_ratings.append('\n                0')
+
+
+# In[100]:
+
+
+test_df = pd.DataFrame({'movie': names,
+                       'date': dates,
+                       'producer': producers,
+                       'actor': actors,
+                       'press_rating': press_ratings,
+                       'spectators_rating' : spectators_ratings})
+print(test_df.info())
+test_df
+
